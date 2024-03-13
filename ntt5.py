0bf8649ba174abdd0246b024502bd6c2dee7cc4d
jammychiou1 <jammy.chiou1@gmail.com>
2024-03-13 11:06:23 +0800

    new style in ntt5 and ntt5_nof...

--- ntt5.py
+++ ntt5.py
@@ -1,143 +1,98 @@
 from params import w
 
 w5 = w ** (4590 // 5)
+red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
+green = 2 * (w5 - w5 ** 4)
+blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
+yellow = 2 * (w5 ** 3 - w5 ** 2)
+
+def ntt5_4x(f0, f1, f2, f3, f4):
+    v0, v1, v2, v3, v4 = f0, f1, f2, f4, f3
+    v1, v3 = v1 + v3, v1 - v3
+    v2, v4 = v2 + v4, v2 - v4
+
+    v1, v2 = v1 + v2, v1 - v2
+    v0, v1 = v0 + v1, 4 * v0 - v1
+    v2 = red * v2
+    v0 = 4 * v0
+    v1, v2 = v1 + v2, v1 - v2
+
+    v3, v4, v5 = v3, v3 + v4, v4
+    v3, v4, v5 = green * v3, blue * v4, yellow * v5
+    v3, v4 = v3 - v5, v4 - v3 - v5
+
+    v1, v3 = v1 + v3, v1 - v3
+    v2, v4 = v2 + v4, v2 - v4
+
+    h0, h1, h3, h4, h2 = v0, v1, v2, v3, v4
+
+    return h0, h1, h2, h3, h4
 
 def ntt5_4x_nof14(f0, f2, f3):
-    red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
-    green = 2 * (w5 - w5 ** 4)
-    blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
-    yellow = 2 * (w5 ** 3 - w5 ** 2)
+    v0, v2, v4 = f0, f2, f3
+    v2, v4 = v2 + v4, v2 - v4
 
-    ci1 = f2 + f3
-    ct0i = ci1
-    h0 = 4 * (f0 + ct0i)
-    ct0i -= 4 * f0
-    ct0o = -ct0i
-    ct1i = -ci1
-    ct1o = red * ct1i
-    co0 = ct0o + ct1o
-    co1 = ct0o - ct1o
+    v1, v2 = v2, -v2
+    v0, v1 = v0 + v1, 4 * v0 - v1
+    v2 = red * v2
+    v0 = 4 * v0
+    v1, v2 = v1 + v2, v1 - v2
 
-    ni1 = f2 - f3
-    nt1i = ni1
-    nt1o = blue * nt1i
-    nt2i = ni1
-    nt2o = yellow * nt2i
-    no0 = -nt2o
-    no1 = nt1o - nt2o
+    v4, v5 = v4, v4
+    v4, v5 = blue * v4, yellow * v5
+    v3, v4 = -v5, v4 - v5
 
-    h1 = co0 + no0
-    h3 = co1 + no1
-    h4 = co0 - no0
-    h2 = co1 - no1
+    v1, v3 = v1 + v3, v1 - v3
+    v2, v4 = v2 + v4, v2 - v4
+
+    h0, h1, h3, h4, h2 = v0, v1, v2, v3, v4
 
     return h0, h1, h2, h3, h4
 
 def ntt5_4x_nof023(f1, f4):
-    red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
-    green = 2 * (w5 - w5 ** 4)
-    blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
-    yellow = 2 * (w5 ** 3 - w5 ** 2)
+    v1, v3 = f1, f4
+    v1, v3 = v1 + v3, v1 - v3
 
-    ci0 = f1 + f4
-    ct0i = ci0
-    h0 = 4 * ct0i
-    ct0o = -ct0i
-    ct1i = ci0
-    ct1o = red * ct1i
-    co0 = ct0o + ct1o
-    co1 = ct0o - ct1o
+    v1, v2 = v1, v1
+    v0, v1 = v1, -v1
+    v2 = red * v2
+    v0 = 4 * v0
+    v1, v2 = v1 + v2, v1 - v2
 
-    ni0 = f1 - f4
-    nt0i = ni0
-    nt0o = green * nt0i
-    nt1i = ni0
-    nt1o = blue * nt1i
-    no0 = nt0o
-    no1 = nt1o - nt0o
+    v3, v4 = v3, v3
+    v3, v4 = green * v3, blue * v4
+    v3, v4 = v3, v4 - v3
 
-    h1 = co0 + no0
-    h3 = co1 + no1
-    h4 = co0 - no0
-    h2 = co1 - no1
+    v1, v3 = v1 + v3, v1 - v3
+    v2, v4 = v2 + v4, v2 - v4
+
+    h0, h1, h3, h4, h2 = v0, v1, v2, v3, v4
 
     return h0, h1, h2, h3, h4
 
 def ntt5_4x_nof03(f1, f2, f4):
-    red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
-    green = 2 * (w5 - w5 ** 4)
-    blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
-    yellow = 2 * (w5 ** 3 - w5 ** 2)
+    v1, v2, v3 = f1, f2, f4
+    v1, v3 = v1 + v3, v1 - v3
+    v2, v4 = v2, v2
 
-    ci0 = f1 + f4
-    ci1 = f2
-    ct0i = ci0 + ci1
-    h0 = 4 * ct0i
-    ct0o = -ct0i
-    ct1i = ci0 - ci1
-    ct1o = red * ct1i
-    co0 = ct0o + ct1o
-    co1 = ct0o - ct1o
+    v1, v2 = v1 + v2, v1 - v2
+    v0, v1 = v1, -v1
+    v2 = red * v2
+    v0 = 4 * v0
+    v1, v2 = v1 + v2, v1 - v2
 
-    ni0 = f1 - f4
-    ni1 = f2
-    nt0i = ni0
-    nt1i = ni0 + ni1
-    nt2i = ni1
-    nt0o = green * nt0i
-    nt1o = blue * nt1i
-    nt2o = yellow * nt2i
-    no0 = nt0o - nt2o
-    no1 = nt1o - nt0o - nt2o
+    v3, v4, v5 = v3, v3 + v4, v4
+    v3, v4, v5 = green * v3, blue * v4, yellow * v5
+    v3, v4 = v3 - v5, v4 - v3 - v5
 
-    h1 = co0 + no0
-    h3 = co1 + no1
-    h4 = co0 - no0
-    h2 = co1 - no1
+    v1, v3 = v1 + v3, v1 - v3
+    v2, v4 = v2 + v4, v2 - v4
 
-    return h0, h1, h2, h3, h4
-
-def ntt5_4x(f0, f1, f2, f3, f4):
-    red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
-    green = 2 * (w5 - w5 ** 4)
-    blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
-    yellow = 2 * (w5 ** 3 - w5 ** 2)
-
-    ci0 = f1 + f4
-    ci1 = f2 + f3
-    ct0i = ci0 + ci1
-    h0 = 4 * (f0 + ct0i)
-    ct0i -= 4 * f0
-    ct0o = -ct0i
-    ct1i = ci0 - ci1
-    ct1o = red * ct1i
-    co0 = ct0o + ct1o
-    co1 = ct0o - ct1o
-
-    ni0 = f1 - f4
-    ni1 = f2 - f3
-    nt0i = ni0
-    nt1i = ni0 + ni1
-    nt2i = ni1
-    nt0o = green * nt0i
-    nt1o = blue * nt1i
-    nt2o = yellow * nt2i
-    no0 = nt0o - nt2o
-    no1 = nt1o - nt0o - nt2o
-
-    h1 = co0 + no0
-    h3 = co1 + no1
-    h4 = co0 - no0
-    h2 = co1 - no1
+    h0, h1, h3, h4, h2 = v0, v1, v2, v3, v4
 
     return h0, h1, h2, h3, h4
 
 def ntt5_4x_noh04(f0, f1, f2, f3, f4):
-    red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
-    green = 2 * (w5 - w5 ** 4)
-    blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
-    yellow = 2 * (w5 ** 3 - w5 ** 2)
-
     ci0 = f1 + f4
     ci1 = f3 + f2
     ct0i = ci0 + ci1
@@ -168,11 +123,6 @@ def ntt5_4x_noh04(f0, f1, f2, f3, f4):
     return h1, h2, h3
 
 def ntt5_4x_noh23(f0, f1, f2, f3, f4):
-    red = w5 + w5 ** 4 - w5 ** 3 - w5 ** 2
-    green = 2 * (w5 - w5 ** 4)
-    blue = 2 * (w5 - w5 ** 4 + w5 ** 3 - w5 ** 2)
-    yellow = 2 * (w5 ** 3 - w5 ** 2)
-
     ci0 = f1 + f4
     ci1 = f3 + f2
     ct0i = ci0 + ci1
